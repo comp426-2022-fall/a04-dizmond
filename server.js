f5fe24178447f608342d3e4096776b4293bbc6e8
dizmond <dizmond@live.unc.edu>
2022-11-30 13:48:37 -0500

    resolving errors

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // endpoint at '/app' should return 200 OK
 app.get('/app', (req, res) => {
     res.status(200).send('200 OK');
@@ -18,10 +20,10 @@ app.get('/app/roll/', (req, res) => {
     res.send(JSON.stringify(json_file));
 });
 
-app.post('/app/roll', (req, res) => {
-    const json_file = roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls))
-    res.send(JSON.stringify(json_file));
-})
+app.post('/app/roll', (req, res, next) => {
+    const json_file = roll(sides, parseInt(req.body.dice), parseInt(req.body.rolls));
+    res.status(200).send(json_file);
+});
 
 app.get('/app/roll/:sides', (req, res) => {
     const json_file = roll(parseInt(req.params.sides), 2, 1);
